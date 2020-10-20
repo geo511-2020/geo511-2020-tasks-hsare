14aed69bebdac2376f70fbc503a4c514b378f95b
hsare <54661479+hsare@users.noreply.github.com>
2020-10-20 14:37:07 -0400

    Case Study7

--- week_07/case_study_07.R
+++ week_07/case_study_07.R
@@ -13,14 +13,14 @@ ggplot(world,aes(x=gdpPercap, y=continent, color=continent))+
 #My New Codes to perform the task
 library(tidyverse) #I did not ued this one
 library(reprex) #I did not ued this one
-library(sf) #I did not ued this one
+library(sf) #I did not used this one
 
 library(spData)
-library(rgdal)
+library(rgdal) #Add
 library(sf)
-library(ggplot2)
+library(ggplot2) #Add
 data(world)
-ggplot(world, aes(x = gdpPercap, fill = continent)) +
+ggplot(world, aes(x = gdpPercap, fill = continent)) + #I replace "y=continent, color=continent" by "fill=continent"
   geom_density(alpha = 0.5, color = F) +
-  labs(x = "GDP Per Capita", y = "Density") +
-  theme(legend.position = "bottom")
+  labs(x = "GDP Per Capita", y = "Density") + #Add this line for x-axis and y-axis
+  theme(legend.position = "bottom") #Add legend
